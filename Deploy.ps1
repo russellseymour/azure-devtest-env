0bd911ba7c308eab87295ca6f9a0be73e1efcd28
tcsatheesh[MSFT] <tc.satheesh@microsoft.com>
2015-07-24 14:44:03 +0100

    Updates

--- Deploy.ps1
+++ Deploy.ps1
@@ -4,32 +4,35 @@ New-AzureResourceGroup -Name "az-dev-net" -Location "North Europe" `
  -TemplateParameterFile "C:\Projects\GitHub\azure-devtest-samples\01-az-dev-net\azuredeploy.parameters.json" `
  -Verbose -Force
 
-#Create the dns server resource group
-New-AzureResourceGroup -Name "az-dns-grp" -Location "North Europe" `
- -TemplateFile "c:\projects\GitHub\Samples\02-az-dns-srv\azuredeploy.json" `
- -TemplateParameterFile "c:\projects\GitHub\Samples\02-az-dns-srv\azuredeploy.parameters.json" `
- -devVNETResourceGroup "az-dev-net" -devVNETVirtualNetworkName "devnet" `
- -Verbose -Force
-  
-#Create the management resource group
-New-AzureResourceGroup -Name "az-mgmt-grp" -Location "North Europe" `
- -TemplateFile "c:\projects\GitHub\Samples\03-az-mgmt-grp\azuredeploy.json" `
- -TemplateParameterFile "c:\projects\GitHub\Samples\03-az-mgmt-grp\azuredeploy.parameters.json" `
- -Verbose -Force
-
 #Create a workstation to remote in and test stuff 
 $secPwd = ConvertTo-SecureString "Corp123!" -AsPlainText -Force
 New-AzureResourceGroup -Name "az-wrk-stn" -Location "North Europe" `
--TemplateFile "c:\projects\GitHub\Samples\04-az-wrk-stn\azuredeploy.json" `
--TemplateParameterFile "c:\projects\GitHub\Samples\04-az-wrk-stn\azuredeploy.parameters.json" `
+-TemplateFile "c:\projects\GitHub\azure-devtest-samples\02-az-wrk-stn\azuredeploy.json" `
+-TemplateParameterFile "c:\projects\GitHub\azure-devtest-samples\02-az-wrk-stn\azuredeploy.parameters.json" `
 -appSubnetName "subnetFrontEnd" -adminPassword $secPwd  `
 -Verbose -Force  
+
+#Create the dns server resource group
+$secPwd = ConvertTo-SecureString "Corp123!" -AsPlainText -Force
+New-AzureResourceGroup -Name "az-dns-grp" -Location "North Europe" `
+ -TemplateFile "c:\projects\GitHub\azure-devtest-samples\03-az-dns-srv\azuredeploy.json" `
+ -TemplateParameterFile "c:\projects\GitHub\azure-devtest-samples\03-az-dns-srv\azuredeploy.parameters.json" `
+ -adminPassword $secPwd  `
+ -devVNETResourceGroup "az-dev-net" -devVNETVirtualNetworkName "devnet" `
+ -Verbose -Force
+
+  
+#Create the management resource group
+New-AzureResourceGroup -Name "az-mgmt-grp" -Location "North Europe" `
+ -TemplateFile "c:\projects\GitHub\azure-devtest-samples\03-az-mgmt-grp\azuredeploy.json" `
+ -TemplateParameterFile "c:\projects\GitHub\azure-devtest-samples\03-az-mgmt-grp\azuredeploy.parameters.json" `
+ -Verbose -Force
 
 #Creat the developer environment resource group
 $secPwd = ConvertTo-SecureString "Corp123!" -AsPlainText -Force
 New-AzureResourceGroup -Name "az-dev-101" -Location "North Europe" `
--TemplateFile "c:\projects\GitHub\Samples\05-az-dev-xxx\azuredeploy.json" `
--TemplateParameterFile "c:\projects\GitHub\Samples\05-az-dev-xxx\azuredeploy.parameters.json" `
+-TemplateFile "c:\projects\GitHub\azure-devtest-samples\05-az-dev-xxx\azuredeploy.json" `
+-TemplateParameterFile "c:\projects\GitHub\azure-devtest-samples\05-az-dev-xxx\azuredeploy.parameters.json" `
 -appSubnetName "subnetdev101" -storageName "asdev101" -vmName "asaz-devweb-101" `
 -adminPassword $secPwd -sizeOfDiskInGB 40 `
 -Verbose -Force  
