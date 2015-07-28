9bfc1a919f6bdf8daf4c7935917bf4ded9b620b2
tcsatheesh[MSFT] <tc.satheesh@microsoft.com>
2015-07-28 16:49:47 +0100

    removed archive folder

--- 99-archive/TearDown.ps1
+++
@@ -1,5 +0,0 @@
-﻿Remove-AzureResourceGroup -Name app1 -Force -Verbose
-Remove-AzureResourceGroup -Name app2 -Force -Verbose
-Remove-AzureResourceGroup -Name app3 -Force -Verbose
-Remove-AzureResourceGroup -Name mgmt -Force -Verbose
-Remove-AzureResourceGroup -Name devVNET -Force -Verbose
