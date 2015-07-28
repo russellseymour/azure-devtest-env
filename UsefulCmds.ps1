c2649f0ae7551de6a97d7285e3d34d04213a4ac5
tcsatheesh[MSFT] <tc.satheesh@microsoft.com>
2015-07-28 16:39:56 +0100

    update

--- UsefulCmds.ps1
+++ UsefulCmds.ps1
@@ -1,3 +1,5 @@
-Get-AzureVM | Format-Table -Property Name, @{Label = "vmSize"; Expression = {$_.HardwareProfileze}}
+Get-AzureVM | Format-Table -Property Name, @{Label = "vmSize"; Expression = {$_.HardwareProfile.VirtualMachineSize}}
 Get-AzureVM | Stop-AzureVM -Force -Verbose
 Get-AzureVM | Format-Table -Property Name, ProvisioningState
+
+Get-AzureNetworkInterface | Sort-Object -Property "Name" | Format-Table -Property ResourceGroupName, Name, @{Label = "PrivateIPAddress"; Expression={$_.IpConfigurations[0].PrivateIPAddress}}
