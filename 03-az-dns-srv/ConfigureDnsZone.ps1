d926cc4babfc19d3622213b78c833cbc49841207
tcsatheesh[MSFT] <tc.satheesh@microsoft.com>
2015-07-29 23:34:14 +0100

    updated zone info

---
+++ 03-az-dns-srv/ConfigureDnsZone.ps1
@@ -0,0 +1,61 @@
+Configuration ConfigureDNSPrimaryZone
+{
+  param ($MachineName)
+
+  Node $MachineName
+  {
+    #Install the DNS Role
+    WindowsFeature DNSFeature
+    {
+      Ensure = "Present"
+      Name = "DNS"
+    }
+    WindowsFeature DNSTools
+    {
+      Ensure = "Present"
+      Name = "RSAT-DNS-Server"
+      DependsOn               = "[WindowsFeature]DNSFeature"
+    }
+    Script ConfigureForwardZone
+    {
+        SetScript = {
+          Add-DnsServerPrimaryZone -Name "custcom.local" -ZoneFile "custcom.local.dns"
+          Set-DnsServerPrimaryZone -Name "custcom.local" -DynamicUpdate "NonsecureAndSecure" -PassThru
+        }
+        #Dummy test script to force the set script to run.
+        TestScript              = { return $false }
+        GetScript               = { <# This must return a hash table #> }
+        DependsOn               = "[WindowsFeature]DNSTools"
+    }
+  }
+}
+Configuration ConfigureDNSSecondaryZone
+{
+  param ($MachineName)
+
+  Node $MachineName
+  {
+    #Install the DNS Role
+    WindowsFeature DNSFeature
+    {
+      Ensure = "Present"
+      Name = "DNS"
+    }
+    WindowsFeature DNSTools
+    {
+      Ensure = "Present"
+      Name = "RSAT-DNS-Server"
+      DependsOn               = "[WindowsFeature]DNSFeature"
+    }
+    Script ConfigureForwardZone
+    {
+        SetScript = {
+          Add-DnsServerSecondaryZone -Name "custcom.local" -ZoneFile "custcom.local.dns" -MasterServers 10.208.2.4
+        }
+        #Dummy test script to force the set script to run.
+        TestScript              = { return $false }
+        GetScript               = { <# This must return a hash table #> }
+        DependsOn               = "[WindowsFeature]DNSTools"
+    }
+  }
+}
