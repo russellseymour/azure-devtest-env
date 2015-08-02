1d696e106768ee753e27058180e6631385199a94
tcsatheesh[MSFT] <tc.satheesh@microsoft.com>
2015-08-02 16:57:51 +0100

    adding dns server configuration

--- 03-az-dns-srv/ConfigurePrimaryDnsZone.ps1
+++ 03-az-dns-srv/ConfigurePrimaryDnsZone.ps1
@@ -16,5 +16,16 @@ Configuration ConfigurePrimaryZone
       Name = "RSAT-DNS-Server"
       DependsOn               = "[WindowsFeature]DNSFeature"
     }
+    Script ConfigureForwardZone
+    {
+        SetScript = {
+          Add-DnsServerPrimaryZone -Name "custcom.local" -ZoneFile "custcom.local.dns" -DynamicUpdate 'NonsecureAndSecure'
+          Set-DnsServerPrimaryZone -Name "custcom.local"  -SecondaryServer 10.208.2.5 -SecureSecondaries 'TransferToSecureServers'  -PassThru
+        }
+        #Dummy test script to force the set script to run.
+        TestScript              = { return $false }
+        GetScript               = { <# This must return a hash table #> }
+        DependsOn               = "[WindowsFeature]DNSTools"
+    }
   }
 }
