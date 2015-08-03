ac6f63730a48a65e5d57d80aa5c4d8d4271ed40d
tcsatheesh[MSFT] <tc.satheesh@microsoft.com>
2015-08-03 10:05:32 +0100

    added workflow to start vms.

--- UsefulCmds.ps1
+++ UsefulCmds.ps1
@@ -6,7 +6,7 @@ Get-AzureNetworkInterface | Sort-Object -Property "Name" | Format-Table -Propert
 
 ((Get-AzureVM -ResourceGroupName "asos-devtest-poc-dns-grp" -Name "dnssrv-11").ResourceExtensionStatusList | Where-Object { $_.HandlerName -eq 'Microsoft.PowerShell.DSC' }).ExtensionSettingStatus.Status
 
-$vmsToShutdown = @('av-01','chef-01','dev101sql-01','dev101utl-01', `
+$vmsList = @('av-01','chef-01','dev101sql-01','dev101utl-01', `
 'dev101web-01','devdc-01','devdc-02','dnssrv-11', `
 'dnssrv-21','es-01','es-02','fs-01','mon-01','mon-02', `
 'prov-01','tst101apa-01','tst101apa-02', `
@@ -19,15 +19,31 @@ $vmsToShutdown = @('dnssrv-11','dnssrv-21')
 
 #Note: You will need to run this workflow in the 64 bit PowerShell which is available under c:\windows\system32\WindowsPowerShell
 workflow ShutdownAzureVMs {
-    param([string[]]$vmsToShutdown)
+    param([string[]]$vmsList)
     $vms = Get-AzureVM
     Foreach -parallel ($vm in $vms)
     {
-        Foreach ($vmName in $vmsToShutdown)
+        Foreach ($vmName in $vmsList)
         {
             if ( $vmName -eq $vm.Name) {
-                    (InlineScript { Write-Host "Shutting down... $Using:vmName"})
+                    (InlineScript { Write-Output "Shutting down... $Using:vmName"})
                     Stop-AzureVM -ResourceGroupName $vm.ResourceGroupName -Name $vmName  -Force -Verbose
+                    #Write-Host "$vmName has been shut down"
+            }
+        }
+    }
+}
+
+workflow StartAzureVMs {
+    param([string[]]$vmsList)
+    $vms = Get-AzureVM
+    Foreach -parallel ($vm in $vms)
+    {
+        Foreach ($vmName in $vmsList)
+        {
+            if ( $vmName -eq $vm.Name) {
+                    (InlineScript { Write-Output "Starting up... $Using:vmName"})
+                    #Start-AzureVM -ResourceGroupName $vm.ResourceGroupName -Name $vmName -Verbose
                     #Write-Host "$vmName has been shut down"
             }
         }
